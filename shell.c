bdb8ace80b17b448293dd6d9082162a107a102ad
Osmir Mariano <osmirmariano@uft.edu.br>
2017-03-17 15:54:05 -0300

    Novas Atualizações
    Correções de bugs presente no código

--- shell.c
+++ shell.c
@@ -3,25 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE 80 /* Definindo o tamanho máximo do comando */
+#define MAX 80 /*Definindo o tamanho máximo do comando*/
 #define TAM 50
-#define buffer "\nHistórico de comando do Shell"
 
 char historico[6][TAM]; //Uma matriz de char para armazenar o histórico
 int cont = 0; //Contador
 
 /*-----------------------------------FUNÇÃO PRINCIPAL----------------------------------*/
 int main(){
-    char armazenaEntrada[MAX_LINE]; /*Para armazenar a entrada do comando*/
-    char *args[MAX_LINE/2+1]; /*Definindo a quantidade de argumentos máximos */
-    int should_run = 1, flag; //a flag para fazer tratamento quando for igual a 1 se o comando é seguido por "&"
- 
-    pid_t pid,tpid;
-    int i;
-   
+    char armazenaEntrada[MAX]; /*Para armazenar a entrada do comando*/
+    char *args[MAX/2+1]; /*Definindo a quantidade de argumentos máximos */
+    int should_run = 1, flag, i; 
+    pid_t pid, tpid;
+
+    printf("\n\nPara visualizar o histórico do terminal, digite 'historico'\n\n");
     while (should_run){ //Esse while com o paramentro should_run cria um loop infinito para o Shell até que o usuário pressione Ctrl+C para sair      
         flag = 0; //flag = 0 por padrão
-        printf("meuShell>");
+        printf("terminal>");
         fflush(stdout);
         if(-1 != comandosShell(armazenaEntrada, args, &flag)){ // condição para obter o próximo comando  
 		    pid = fork();   
@@ -40,14 +38,12 @@ int main(){
             	i++;
            	 	if (flag == 0){
                 	i++;
-                	//wait(NULL);
+                	wait(NULL);
            		 }
         	}
    	    }
      }
 }
-
-
 
 /*-------------------------------FUNÇÃO QUE IRÁ EXIBIR O HISTÓRICO DO SHELL----------------------------------*/
 void historicoShell(){
@@ -70,13 +66,12 @@ void historicoShell(){
 } 
 
 /*--------------------FUNÇÃO UTILIZADA PARA PEGAR OS COMANDOS DO SHELL E TRAMENTO DO HISTÓRICO--------------------*/
-//Essa função é utilizada para pegar os comandos do Shell
 int comandosShell(char texto[], char *args[], int *flag){
    	int i, tamanho; //Para caracteres na linha de comando
     int inicio;  //indice do início do próximo comando
     int ct = 0; // indice de onde colocar o próximo parâmetro args[]
 
-    tamanho = read(STDIN_FILENO, texto, MAX_LINE);//tamanho recebe a quantidade de dados lido pela função read
+    tamanho = read(STDIN_FILENO, texto, MAX);//tamanho recebe a quantidade de dados lido pela função read
     inicio = -1;
 
     if (tamanho == 0)
@@ -86,7 +81,7 @@ int comandosShell(char texto[], char *args[], int *flag){
         exit(-1);  //Encerra a execução
     }
     
-   //Examina cada caracter inserido
+    //Examina cada caracter inserido
     for (i = 0; i < tamanho; i++){
         switch (texto[i]){
             case ' ':
@@ -122,7 +117,7 @@ int comandosShell(char texto[], char *args[], int *flag){
 
     //Código abaixo todo se refere ao tramento do histório. Poderia criar uma outra função para essa, mas vamos ficar assim mesmo.
     //Condição para chamar a função histórico e demais questões relacionado a ele
-    if(strcmp(args[0],"historico")==0){		
+    if(strcmp(args[0], "historico") == 0){		
         if(cont > 0){
             historicoShell();
 	    }
@@ -138,22 +133,17 @@ int comandosShell(char texto[], char *args[], int *flag){
 		if(x > cont) { 
 		    printf("\nNão existe esse comando no histórico\n");
 		    strcpy(texto,"Comando Errado");
-		} 
-		else if (z != -48){
-		    printf("\nO comando do histórico. Digite < =! 5 (tamanho do buffer é 6 junto com o comando atual)\n");
-		    strcpy(texto,"Comando Errado");
 		}
 		else{
-			if(x ==- 15){//Verificando o valor de "!!" na tabela ASCII é 33.
-				 strcpy(texto,historico[0]); 
+			if(x == -15){//Verificando o valor de "!!" na tabela ASCII é 33.
+				 strcpy(texto, historico[0]); 
 			}
 			else if(x == 0){
 				 printf("Insira o comando adequado");
 				strcpy(texto,"Comando Errado");
-			}
-			
+			}			
 			else if(x >= 1){
-				strcpy(texto,historico[cont-x]);
+				strcpy(texto, historico[cont-x]);
 			}
 		}
 	}
@@ -163,7 +153,7 @@ int comandosShell(char texto[], char *args[], int *flag){
     
     strcpy(historico[0], texto); // Atualiza a matriz de histórico com os dados de entrada
     cont++;
-	if(cont > 10)	{ 
-        cont = 10;
+	if(cont > 6)	{ 
+        cont = 6;
 	}
 }
